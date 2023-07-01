29791e7d8391e3a7ea908f8de1f587c85675e7b7
Jason Zhao <jason1382311@gmail.com>
2023-07-01 14:29:28 -0700

    remove bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,6 @@ import "regenerator-runtime/runtime";
 import React, { useEffect, useState } from "react";
 import { login, logout } from "./utils";
 
-// React Bootstrap css
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// React Bootstraps imports
-import { Nav, Navbar, Container, Row, Card, Alert } from "react-bootstrap";
-
 // Custom Components
 import MintingTool from "./Components/MintingTool";
 
@@ -19,29 +13,29 @@ const { networkId } = getConfig(process.env.NODE_ENV || "development");
 export default function App() {
   const [userHasNFT, setuserHasNFT] = useState(false);
 
-  useEffect(() => {
-    const receivedNFT = async () => {
-      console.log(
-        await window.contract.check_token({
-          id: `${window.accountId}-go-team-token`,
-        })
-      );
-      if (window.accountId !== "") {
-        console.log(
-          await window.contract.check_token({
-            id: `${window.accountId}-go-team-token`,
-          })
-        );
+  // useEffect(() => {
+  //   const receivedNFT = async () => {
+  //     console.log(
+  //       await window.contract.check_token({
+  //         id: `${window.accountId}-go-team-token`,
+  //       })
+  //     );
+  //     if (window.accountId !== "") {
+  //       console.log(
+  //         await window.contract.check_token({
+  //           id: `${window.accountId}-go-team-token`,
+  //         })
+  //       );
 
-        setuserHasNFT(
-          await window.contract.check_token({
-            id: `${window.accountId}-go-team-token`,
-          })
-        );
-      }
-    };
-    receivedNFT();
-  }, []);
+  //       setuserHasNFT(
+  //         await window.contract.check_token({
+  //           id: `${window.accountId}-go-team-token`,
+  //         })
+  //       );
+  //     }
+  //   };
+  //   receivedNFT();
+  // }, []);
 
   return (
     <div>
