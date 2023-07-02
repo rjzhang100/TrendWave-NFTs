60d94868689f8c07a8959dc001b17289757a907f
Frank Jin <fjin@konradgroup.com>
2023-07-02 03:15:34 -0700

    ipfs stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import "regenerator-runtime/runtime";
 import React, { useEffect, useState } from "react";
+
 import { login, logout } from "./utils";
+
+
 import "./App.css";
 
 // Custom Components
@@ -18,132 +21,132 @@ const { networkId } = getConfig(process.env.NODE_ENV || "development");
 adminAccountID = "lejin.testnet";
 
 export default function App() {
-  const [nfts, setNfts] = useState([]);
+	const [nfts, setNfts] = useState([]);
 
-  useEffect(() => {
-    const getNfts = async () => {
-      if (window.accountId) {
-        setNfts(
-          await window.contract.nft_tokens_for_owner({
-            account_id: adminAccountID,
-          })
-        );
-      }
-    };
-    getNfts();
-  }, []);
+	useEffect(() => {
+		const getNfts = async () => {
+			if (window.accountId) {
+				setNfts(
+					await window.contract.nft_tokens_for_owner({
+						account_id: adminAccountID,
+					})
+				);
+			}
+		};
+		getNfts();
+	}, []);
 
-  const checkNFT = async () => {
-    console.log(window.walletConnection.getAccountId());
-    // not sure why its not working
-    const nft = await window.contract.nft_tokens_for_owner({
-      account_id: window.accountId,
-    });
-    console.log(nft);
-  };
+	const checkNFT = async () => {
+		console.log(window.walletConnection.getAccountId());
+		// not sure why its not working
+		const nft = await window.contract.nft_tokens_for_owner({
+			account_id: window.accountId,
+		});
+		console.log(nft);
+	};
 
-  console.log(nfts);
+	console.log(nfts);
 
-  return (
-    <div>
-      <div
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "0",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-end",
-          flexDirection: "row",
-          width: "100vw",
-          height: "75px",
-          backgroundColor: "grey",
-          zIndex: "12312",
-        }}
-      >
-        <div
-          style={{
-            marginRight: "auto",
-            marginLeft: "25px",
-            fontSize: "30px",
-            fontWeight: "bold",
-          }}
-        >
-          TrendWave NFT
-        </div>
-        <button
-          style={{ width: "150px", marginRight: "25px" }}
-          onClick={window.walletConnection.isSignedIn() ? logout : login}
-        >
-          {window.walletConnection.isSignedIn() ? window.accountId : "Login"}
-        </button>
-      </div>
-      {window.innerWidth > 600 && (
-        <div
-          style={{
-            backgroundColor: "lightgrey",
-            position: "absolute",
-            left: "0",
-            height: "100%",
-            width: "150px",
-            paddingTop: "85px",
-            color: "black",
-            display: "flex",
-            flexDirection: "column",
-            gap: "15px",
-          }}
-        >
-          <div style={{ marginTop: "15px" }}>
-            <div style={{ marginBottom: "10px" }}>Sort NFTs by Category</div>
-            <div
-              style={{
-                width: "100%",
-                height: "25px",
-                backgroundColor: "white",
-                color: "black",
-                textAlign: "center",
-              }}
-            >
-              Funny
-            </div>
-          </div>
-          <div>
-            <div>Sort NFTs by Price</div>
-            <div
-              style={{
-                width: "100%",
-                height: "25px",
-                backgroundColor: "white",
-                color: "black",
-                textAlign: "center",
-              }}
-            >
-              Lowest to Highest
-            </div>
-          </div>
-          <div>
-            Get Trendy NFT For:
-            <div
-              style={{
-                width: "100%",
-                height: "25px",
-                backgroundColor: "white",
-                color: "black",
-                textAlign: "center",
-              }}
-            >
-              July 2 2023
-            </div>
-          </div>
-        </div>
-      )}
-      <div
-        style={{
-          paddingLeft: window.innerWidth > 600 ? "150px" : "0px",
-        }}
-      >
-        <ImageGallery nfts={nfts} />
-        {/* <h1>Your NFTs</h1>
+	return (
+		<div>
+			<div
+				style={{
+					position: "absolute",
+					top: "0",
+					left: "0",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "flex-end",
+					flexDirection: "row",
+					width: "100vw",
+					height: "75px",
+					backgroundColor: "grey",
+					zIndex: "12312",
+				}}
+			>
+				<div
+					style={{
+						marginRight: "auto",
+						marginLeft: "25px",
+						fontSize: "30px",
+						fontWeight: "bold",
+					}}
+				>
+					TrendWave NFT
+				</div>
+				<button
+					style={{ width: "150px", marginRight: "25px" }}
+					onClick={window.walletConnection.isSignedIn() ? logout : login}
+				>
+					{window.walletConnection.isSignedIn() ? window.accountId : "Login"}
+				</button>
+			</div>
+			{window.innerWidth > 600 && (
+				<div
+					style={{
+						backgroundColor: "lightgrey",
+						position: "absolute",
+						left: "0",
+						height: "100%",
+						width: "150px",
+						paddingTop: "85px",
+						color: "black",
+						display: "flex",
+						flexDirection: "column",
+						gap: "15px",
+					}}
+				>
+					<div style={{ marginTop: "15px" }}>
+						<div style={{ marginBottom: "10px" }}>Sort NFTs by Category</div>
+						<div
+							style={{
+								width: "100%",
+								height: "25px",
+								backgroundColor: "white",
+								color: "black",
+								textAlign: "center",
+							}}
+						>
+							Funny
+						</div>
+					</div>
+					<div>
+						<div>Sort NFTs by Price</div>
+						<div
+							style={{
+								width: "100%",
+								height: "25px",
+								backgroundColor: "white",
+								color: "black",
+								textAlign: "center",
+							}}
+						>
+							Lowest to Highest
+						</div>
+					</div>
+					<div>
+						Get Trendy NFT For:
+						<div
+							style={{
+								width: "100%",
+								height: "25px",
+								backgroundColor: "white",
+								color: "black",
+								textAlign: "center",
+							}}
+						>
+							July 2 2023
+						</div>
+					</div>
+				</div>
+			)}
+			<div
+				style={{
+					paddingLeft: window.innerWidth > 600 ? "150px" : "0px",
+				}}
+			>
+				<ImageGallery nfts={nfts} />
+				{/* <h1>Your NFTs</h1>
       {nfts.map((nft, index) => {
         return (
           <div key={index}>
@@ -151,16 +154,16 @@ export default function App() {
           </div>
         );
       })} */}
-        {/* <button
+				{/* <button
         onClick={() => {
           checkNFT();
         }}
       >
         TEST: log nfts
       </button> */}
-        {/* {adminAccountID === window.accountId ? <MintingTool /> : <></>} */}
-		{adminAccountID === window.accountId ? <TrendMintingTool /> : <></>}
-      </div>
-    </div>
-  );
+				{/* {adminAccountID === window.accountId ? <MintingTool /> : <></>} */}
+				{adminAccountID === window.accountId ? <TrendMintingTool /> : <></>}
+			</div>
+		</div>
+	);
 }
--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { NFTStorage, File } from 'nft.storage/dist/bundle.esm.min'
+import mime from 'mime'
+import fs from 'fs'
+import path from 'path'
+const BN = require("bn.js");
 import "../App.css";
 
+const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGFhZUEwQjc5NzlEQUI3YTIwYjQ2RjljNjI1RkVhN0M1YjM0OUFGNEUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4ODI4OTMyNjU2MSwibmFtZSI6InRyZW5kd2F2ZSJ9.FTkIlEGPXmM_LKX-5uUc5ba0DFF935t6zSUK8E1Dz3E'
 const Card = ({ prompt }) => {
 	// nftString is the base64 string of the NFT image
 	const [nftString, setNftString] = useState("");
@@ -20,14 +26,39 @@ const Card = ({ prompt }) => {
 			})
 	}
 
+	const fileFromBase64 = async (b64string) => {
+		const content = Buffer.from(b64string, 'base64');
+		const type = 'image/png'; // Assuming the base64 string represents a PNG file
+		return new File([content], 'image.png', { type });
+	};
+
+	const storeNFT = async (image, name, description) => {
+
+		// create a new NFTStorage client using our API key
+		const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
+
+		// call client.store, passing in the image & metadata
+		return nftstorage.store({
+			image,
+			name,
+			description,
+		})
+	}
+
 	const mintNFT = async (tokenId, description, media) => {
+		const image = await fileFromBase64(media)
+		console.log(image);
+		const res = await storeNFT(image, tokenId, description);
+		const ipfs = res.data.image.href;
+		console.log(ipfs)
+		return
 		await window.contract.nft_mint(
 			{
 				token_id: tokenId,
 				metadata: {
 					title: tokenId,
 					description: description,
-					media: media,
+					media: ipfs,
 				},
 				receiver_id: window.accountId,
 			},
